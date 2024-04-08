81d3f8c69958e5b54ed19508ee8bf47fd65e5414
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2024-04-08 14:02:02 +0300

    comm

--- TrainCSh/Program.cs
+++ TrainCSh/Program.cs
@@ -1,65 +1,68 @@
 ﻿using System;
 using System.Globalization;
 
-namespace トレイン
+namespace トレイン // Train
 {
-    public record 電車(string Destination, int TrainNumber, DateTime DepartureTime)
+    public record 電車(string 行き先, int 列車番号, DateTime 出発時間)
     {
-        public 電車(string destination, int trainNumber, string departureTime) : this(destination, trainNumber, DateTime.ParseExact(departureTime, "HH:mm", new CultureInfo("ja-JP")))
+        public 電車(string 行き先, int 列車番号, string 出発時間) : this(行き先, 列車番号, DateTime.ParseExact(出発時間, "HH:mm", new CultureInfo("ja-JP")))
         {
         }
 
         public override string ToString()
         {
-            return $"列車番号(Train Number): {TrainNumber}, 行き先(Destination): {Destination}, 出発時間(Departure Time): {DepartureTime:HH:mm}";
+            return $"列車番号: {列車番号}, 行き先: {行き先}, 出発時間: {出発時間:HH:mm}";
         }
     }
-    class プログラム
+    class プログラム // Program
     {
         static void Main()
         {
-            電車[] trains =
+            電車[] 電車の配列 =
             [
-                new 電車("神楽坂(Kagurazaka)", 6710, "15:30"),
-                new 電車("市ヶ谷(Ichigaya)", 9814, "14:00"),
-                new 電車("四谷(Yotsuya)", 3125, "16:45"),
-                new 電車("神保町(Jimbocho)", 1638, "13:20"),
-                new 電車("秋葉原(Akihabara)", 6946, "17:05"),
+                new 電車("神楽坂 (カグラザカ)", 6710, "15:30"),
+                new 電車("市ヶ谷 (イチガヤ)", 9814, "14:00"),
+                new 電車("四谷 (ヨツヤ)", 3125, "16:45"),
+                new 電車("神保町 (ジンボウチョウ)", 1638, "13:20"),
+                new 電車("秋葉原 (アキハバラ)", 6946, "17:05"),
             ];
 
-            Array.Sort(trains, (t1, t2) => t1.TrainNumber.CompareTo(t2.TrainNumber));
+            // Сначала сортируем по номеру поезда
+            Array.Sort(電車の配列, (電車1, 電車2) => 電車1.列車番号.CompareTo(電車2.列車番号));
 
-            foreach (var train in trains)
+            // Затем сортируем по направлению и времени отправления
+            Array.Sort(電車の配列, (電車1, 電車2) =>
             {
-                Console.WriteLine(train);
-            }
-
-            Console.Write("列車番号を入力してください(Please enter your train number): ");
-            int trainNumber = Convert.ToInt32(Console.ReadLine());
-            電車 trainInfo = Array.Find(trains, t => t.TrainNumber == trainNumber);
-            if (trainInfo != null)
-            {
-                Console.WriteLine(trainInfo);
-            }
-            else
-            {
-                Console.WriteLine("このレベルの列車はありません.(Please enter your train number:)");
-            }
-
-            Array.Sort(trains, (t1, t2) =>
-            {
-                int destinationComparison = t1.Destination.CompareTo(t2.Destination);
-                if (destinationComparison != 0)
-                    return destinationComparison;
+                int 行き先比較 = 電車1.行き先.CompareTo(電車2.行き先);
+                if (行き先比較 != 0)
+                    return 行き先比較;
                 else
-                    return t1.DepartureTime.CompareTo(t2.DepartureTime);
+                    return 電車1.出発時間.CompareTo(電車2.出発時間);
             });
 
-            Console.WriteLine("目的地と時刻ごとに並べられた列車(Trains sorted by destination and time):");
-            foreach (var train in trains)
+            // Выводим отсортированный список поездов
+            Console.WriteLine("目的地と時刻ごとに並べられた列車:");
+            foreach (var 電車 in 電車の配列)
             {
-                Console.WriteLine(train);
+                Console.WriteLine(電車);
             }
         }
     }
 }
+
+// English Translations for Comments:
+// public record 電車(string 行き先, int 列車番号, DateTime 出発時間) -> public record Train(string Destination, int TrainNumber, DateTime DepartureTime)
+// public Train(string destination, int trainNumber, string departureTime) : this(destination, trainNumber, DateTime.ParseExact(departureTime, "HH:mm", new CultureInfo("ja-JP")))
+// return $"Train Number: {TrainNumber}, Destination: {Destination}, Departure Time: {DepartureTime:HH:mm}";
+// Train[] trains =
+// new Train("Kagurazaka", 6710, "15:30"),
+// new Train("Ichigaya", 9814, "14:00"),
+// new Train("Yotsuya", 3125, "16:45"),
+// new Train("Jimbocho", 1638, "13:20"),
+// new Train("Akihabara", 6946, "17:05"),
+// Array.Sort(trains, (train1, train2) => train1.TrainNumber.CompareTo(train2.TrainNumber));
+// Forward Sort by Train Number
+// Then Sort by Destination and Departure Time
+// Trains sorted by destination and time:
+// foreach (var train in trains)
+// Console.WriteLine(train);
